08748c0c1fcb85e8478c2505e1e9d7b835c4bf21
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-02-16 21:15:06 +0530

    ASSIGNMENT 6 / LEVEL 2

---
+++ RandomStats.java
@@ -0,0 +1,41 @@
+package com.gla.level2;
+
+public class RandomStats {
+
+    public static int[] generate4DigitRandomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1000 + (int)(Math.random() * 9000);
+        }
+        return arr;
+    }
+
+    public static double[] findAverageMinMax(int[] numbers) {
+        int min = numbers[0];
+        int max = numbers[0];
+        int sum = 0;
+
+        for (int x : numbers) {
+            sum += x;
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+
+        double avg = (double) sum / numbers.length;
+        return new double[]{avg, min, max};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = generate4DigitRandomArray(5);
+
+        System.out.print("Numbers: ");
+        for (int x : arr) System.out.print(x + " ");
+        System.out.println();
+
+        double[] result = findAverageMinMax(arr);
+        System.out.println("Average = " + result[0]);
+        System.out.println("Min = " + result[1]);
+        System.out.println("Max = " + result[2]);
+    }
+}
+
