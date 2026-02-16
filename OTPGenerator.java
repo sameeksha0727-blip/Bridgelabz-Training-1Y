90d0ce41504355c154cb03a80af2abb2dd86bf1b
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-02-16 21:33:21 +0530

    ASSIGNMENT 6 / LEVEL 3

---
+++ OTPGenerator.java
@@ -0,0 +1,29 @@
+package com.gla.level3;
+
+import java.util.Random;
+import java.util.HashSet;
+
+public class OTPGenerator {
+
+    public static int generateOTP() {
+        return 100000 + (int)(Math.random() * 900000);
+    }
+
+    public static boolean areUnique(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : arr) set.add(x);
+        return set.size() == arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] otps = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            otps[i] = generateOTP();
+            System.out.println("OTP " + (i + 1) + ": " + otps[i]);
+        }
+
+        System.out.println("All unique? " + areUnique(otps));
+    }
+}
+
