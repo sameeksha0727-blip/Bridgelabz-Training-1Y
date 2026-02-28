a47573521cb7387007f979eeac3d02e19c86f94e
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-02-28 21:18:52 +0530

    ASSIGNMENT 8 / QUES 1

--- Student.java
+++ Student.java
@@ -1,43 +1,16 @@
-package com.gla.level2;
+package college.student;
 
 public class Student {
+    private String name;
+    private int rollNo;
 
-    // Attributes
-    String name;
-    int rollNumber;
-    int marks;
-
-    // Constructor
-    public Student(String name, int rollNumber, int marks) {
+    public Student(String name, int rollNo) {
         this.name = name;
-        this.rollNumber = rollNumber;
-        this.marks = marks;
+        this.rollNo = rollNo;
     }
 
-    // Method to calculate grade
-    public String calculateGrade() {
-        if (marks >= 90)
-            return "A";
-        else if (marks >= 75)
-            return "B";
-        else if (marks >= 60)
-            return "C";
-        else if (marks >= 40)
-            return "D";
-        else
-            return "Fail";
-    }
-
-    // Method to display details
-    public void displayDetails() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll Number: " + rollNumber);
-        System.out.println("Marks: " + marks);
-        System.out.println("Grade: " + calculateGrade());
-    }
-
-    public static void main(String[] args) {
-        Student s1 = new Student("Suhani", 12, 85);
-        s1.displayDetails();
+    public void displayStudent() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
     }
 }
