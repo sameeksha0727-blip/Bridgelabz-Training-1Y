6250663a33a6590f2028cd00d8a98b7ef354abf4
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-01 23:22:18 +0530

    ASSIGNMENT 10

---
+++ Car.java
@@ -0,0 +1,10 @@
+public class Car implements Vehicle {
+
+    public void rent() {
+        System.out.println("Car rented");
+    }
+
+    public void returnVehicle() {
+        System.out.println("Car returned");
+    }
+}
