e9fc84dfe76c3982203de21ba5aa3b9399c4a7ea
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-01 20:55:04 +0530

    ASSIGNMENT 8 / QUES 3

---
+++ StaticDemo.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.*;
+
+public class StaticDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Square Root: " + sqrt(25));
+        System.out.println("Power: " + pow(2,3));
+        System.out.println("Maximum: " + max(10,20));
+        System.out.println("Minimum: " + min(5,3));
+        System.out.println("Absolute: " + abs(-15));
+    }
+}
