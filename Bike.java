28b6feabec9719071654c67d70fe56c4e244d37c
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-01 23:21:54 +0530

    ASSIGNMENT 10

---
+++ Bike.java
@@ -0,0 +1,10 @@
+public class Bike implements Vehicle {
+
+    public void rent() {
+        System.out.println("Bike rented");
+    }
+
+    public void returnVehicle() {
+        System.out.println("Bike returned");
+    }
+}
