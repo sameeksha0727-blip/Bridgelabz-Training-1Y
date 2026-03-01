0ab1d5d72840db496c71efca885e55a666edad5b
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-01 23:20:39 +0530

    ASSIGNMENT 10

---
+++ AC.java
@@ -0,0 +1,10 @@
+public class AC implements SmartDevice {
+
+    public void turnOn() {
+        System.out.println("AC is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("AC is OFF");
+    }
+}
