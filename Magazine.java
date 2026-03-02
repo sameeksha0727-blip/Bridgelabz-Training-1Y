5f7b75e0da6b537ee5d5e82a12cd64fef31fca72
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-02 21:52:20 +0530

    STORY BASED QUES

---
+++ Magazine.java
@@ -0,0 +1,11 @@
+public class Magazine extends Book {
+
+    public Magazine(int bookId, String title, double price) {
+        super(bookId, title, price);
+    }
+
+    @Override
+    public double calculateFine(int daysLate) {
+        return daysLate * 5;
+    }
+}
