a23ffff5c4cd9d704c0cbefcb93075a18e7aa782
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-04 17:28:13 +0530

    EXTRAS / BUILD-IN FUNTIONS

---
+++ NumberGuessingGame.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    static int generateGuess(int low, int high) {
+        return (low + high) / 2;   // Binary Search approach
+    }
+
+    static String getFeedback(Scanner sc) {
+        System.out.print("Is the guess High, Low, or Correct? ");
+        return sc.nextLine().toLowerCase();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int low = 1, high = 100;
+        String feedback = "";
+
+        System.out.println("Think of a number between 1 and 100.");
+
+        while (!feedback.equals("correct")) {
+            int guess = generateGuess(low, high);
+            System.out.println("Computer guesses: " + guess);
+
+            feedback = getFeedback(sc);
+
+            if (feedback.equals("high")) {
+                high = guess - 1;
+            } else if (feedback.equals("low")) {
+                low = guess + 1;
+            }
+        }
+
+        System.out.println("Yay! Computer guessed correctly.");
+        sc.close();
+    }
+}
