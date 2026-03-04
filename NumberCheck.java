790bccd4d220d2922e1882fac8d881b8e5a6048e
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-04 13:35:40 +0530

    ASSIGNMENT 4 / LEVEL 1

---
+++ NumberCheck.java
@@ -0,0 +1,40 @@
+package com.gla.arrays.level1;
+
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] > 0) {
+                if (numbers[i] % 2 == 0)
+                    System.out.println(numbers[i] + " is positive and even");
+                else
+                    System.out.println(numbers[i] + " is positive and odd");
+            }
+            else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is negative");
+            }
+            else {
+                System.out.println("Number is zero");
+            }
+        }
+
+        if (numbers[0] == numbers[4])
+            System.out.println("First and last elements are equal");
+        else if (numbers[0] > numbers[4])
+            System.out.println("First element is greater");
+        else
+            System.out.println("Last element is greater");
+    }
+}
---
+++ VotingEligibility.java
@@ -0,0 +1,30 @@
+package com.gla.arrays.level1;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        System.out.println("Enter age of 10 students:");
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+
+            if (ages[i] < 0) {
+                System.out.println("Invalid age");
+            }
+            else if (ages[i] >= 18) {
+                System.out.println("The student with age " + ages[i] + " can vote.");
+            }
+            else {
+                System.out.println("The student with age " + ages[i] + " cannot vote.");
+            }
+        }
+    }
+}
