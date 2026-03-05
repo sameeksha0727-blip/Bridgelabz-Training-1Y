56d377ab73635901353c4f47867f00d3ea911c41
sameeksha0727-blip <sameeksha0727@gmail.com>
2026-03-05 21:39:24 +0530

    ASSIGNMENT 2 / LEVEL 1

---
+++ Basic-Programs/HandshakesCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HandshakesCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int handshakes = (n * (n - 1)) / 2;
+
+        System.out.println("Maximum number of possible handshakes is " + handshakes);
+
+        sc.close();
+    }
+}
---
+++ Basic-Programs/HeightConverter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HeightConverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter height in cm: ");
+        double cm = sc.nextDouble();
+
+        double inches = cm / 2.54;
+        double feet = inches / 12;
+
+        System.out.println("Your Height in cm is " + cm +
+                " while in feet is " + feet +
+                " and inches is " + inches);
+
+        sc.close();
+    }
+}
